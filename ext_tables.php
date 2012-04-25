b38b2592721b761b4292209717cc799d68dca9ee
Nico de Haen <mail@ndh-websolutions.de>
2012-04-25 21:19:57 +0200

    Removed unused files for FLOW3

--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-
-require_once(t3lib_extMgm::extPath('php_parser_api') . 'Classes/AutoLoader.php');
-Tx_PhpParser_AutoLoader::register();
-?>
